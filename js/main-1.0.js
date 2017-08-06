6e17b06a14c4cf2a57065b7cb3499c32755164ae
IbrahimIrfan <1ibrahimirfan@gmail.com>
2017-08-06 14:44:47 -0400

    edits and added imgs

--- js/main-1.0.js
+++ js/main-1.0.js
@@ -1,8 +1,6 @@
 function appendFooter(){
   var footer = "<div class='footer'>";
-
   footer += "<center><div class='copyrightText'>&copy; 2017 ITEE Group Canada</div></center>";
-
   footer += "</div>";
   $('body').append(footer);
 }
